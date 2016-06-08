8ffed9706653bb0d1d66fce9b45ac18f94cd99b0
scotthso <scotthhso@gmail.com>
2016-06-08 12:54:56 -0400

    implemented full screen mode

---
+++ main.js
@@ -0,0 +1,7 @@
+const elem = document.getElementById('target');
+
+document.getElementById('button').addEventListener('click', () => {
+    if (screenfull.enabled) {
+        screenfull.request();
+    }
+});
