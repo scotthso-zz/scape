d770294074526561481604fca27110bac07a48dc
scotthso <scotthhso@gmail.com>
2016-06-10 13:38:46 -0400

    added second mountain range

--- draw.js
+++ draw.js
@@ -2,18 +2,35 @@
 
 function paint() {
 
+       //Mountain 2
+      ctx.beginPath();
+      ctx.moveTo(0, ch*.7);
+      ctx.lineTo(cw*.2, ch*.65);
+      ctx.lineTo(cw*.4, ch*.7);
+      ctx.lineTo(cw*.6, ch*.55);
+      ctx.lineTo(cw*.8, ch*.7);
+      ctx.lineTo(cw, ch);
+      ctx.lineTo(0,ch);
+      ctx.lineWidth = 1;
+      ctx.strokeStyle = '#98B6A1';
+      ctx.fillStyle = '#98B6A1';    
+      ctx.fill();
+      ctx.stroke();
+      ctx.closePath();
       //Mountain 1
       ctx.beginPath();
-	ctx.moveTo(cw, ch-340);
-      ctx.quadraticCurveTo(cw-200, 150, cw-300, 222);
-      ctx.quadraticCurveTo(cw-600, ch-195, cw-685, ch-195);
-      ctx.lineTo(cw, ch-195);
+	ctx.moveTo(cw, ch*.8);
+      ctx.lineTo(cw*.8, ch*.4);
+      ctx.lineTo(cw*.6, ch);
+      ctx.lineTo(cw, ch);
       ctx.lineWidth = 1;
       ctx.strokeStyle = '#709A7D';
       ctx.fillStyle = '#709A7D';    
       ctx.fill();
       ctx.stroke();
       ctx.closePath();
+
+
 };
 
 
