385441f7c3d588a5ffff5aef21c6b057b9113455
scotthso <scotthhso@gmail.com>
2016-06-24 23:38:36 -0400

    finished off the site with an about popup and title menu

--- main.js
+++ main.js
@@ -5,7 +5,21 @@ const elem = document.getElementById('target');
 document.getElementById('button').addEventListener('click', () => {
     if (screenfull.enabled) {
     		document.getElementById('button').style.display = 'none';
+    		document.getElementById('title').style.display = 'none';
+    		document.getElementById('author').style.display = 'none';
+
+
         screenfull.request();
         //paint();
+    } else {
+    	document.getElementById('button').style.display = 'inherit';
     }
+});
+
+document.getElementById('close').addEventListener('click', () => {
+   document.getElementById('about-popup').style.display = 'none';
+});
+
+document.getElementById('about').addEventListener('click', () => {
+   document.getElementById('about-popup').style.display = 'block';
 });
